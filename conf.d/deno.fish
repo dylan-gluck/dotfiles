0096f11bc6e2e3c7688924d2952203c9e4aa6dd1
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2025-02-01 22:45:02 -0500

    Updated config

---
+++ conf.d/deno.fish
@@ -0,0 +1,1 @@
+source "/Users/dylan/.deno/env.fish"
--- config.fish
+++ config.fish
@@ -1,5 +1,7 @@
 source ~/.config/fish/alias.fish
 
+set -U fish_greeting
+
 function starship_transient_prompt_func
     starship module character
 end
